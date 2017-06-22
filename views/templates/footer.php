bb5feda80b21fd8489116876eda14b9d3ae06237
Reaper45 <jomwashighadi@gmail.com>
2017-06-23 00:47:36 +0300

    Set up footer

---
+++ views/templates/footer.php
@@ -0,0 +1,34 @@
+<?php
+/**
+* Created By : Reaper45
+* Date : 23/06/2017
+* Time : 00:40 AM
+*
+* Footer for every page in this project.
+*/
+?>
+
+
+</div>
+<!-- End container -->
+
+<!-- Page Footer -->
+<footer>
+    <p>&copy; &nbsp;<? echo date(Y); ?> &nbsp; Nelson Otouma.</p>
+</footer>
+<!-- End footer -->
+
+
+<!-- Scripts -->
+<script type="text/javascript">
+
+</script>
+<!-- Jquery -->
+<script type="text/javascript" src=""></script>
+<!-- Bootstrap js -->
+<script type="text/javascript" src=""></script>
+<!-- Custom js -->
+<script type="text/javascript" src=""></script>
+
+</body>
+</html>
