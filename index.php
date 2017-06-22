70c4cf960bbe288775491761dc243f08b8099a16
Reaper45 <jomwashighadi@gmail.com>
2017-06-23 00:39:29 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Created By : Reaper45
+* Date : 23/06/2017
+* Time : 00:25 AM
+*
+* Project Entry
+*/
+
+require_once 'file';
---
+++ views/templates/header.php
@@ -0,0 +1,26 @@
+<?php
+/**
+* Created By : Reaper45
+* Date : 23/06/2017
+* Time : 00:29 AM
+*
+* Header for every page in this project.
+*/
+?>
+
+<html>
+<head>
+    <!-- Page title -->
+    <title> <? echo APP_NAME." | ".$title; ?> </title>
+
+    <!-- Css links -->
+    <link rel="stylesheet" href="<? echo BASE_URL;?>/css/master.css">
+
+</head>
+<body>
+    <!-- Navigation -->
+
+    <!-- Navigation End -->
+
+    <!-- Container -->
+    <div class="container">
