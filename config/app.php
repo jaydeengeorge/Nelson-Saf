29f9bbb8a7f38feb360597c7a220cac396ece825
Reaper45 <jomwashighadi@gmail.com>
2017-06-23 01:07:08 +0300

    Project configurations

---
+++ config/app.php
@@ -0,0 +1,15 @@
+<?php
+/**
+* Created By : Reaper45
+* Date : 23/06/2017
+* Time : 00:54 AM
+*
+*/
+
+ // Database connection configuration variables
+
+ // Application Name ( Feel free to cahnge)
+ define('APP_NAME', 'Safaricom' );
+
+// Define base url. Will come in hady when building the front end
+ define('BASE_URL', 'http://localhost' );
---
+++ config/database-example.php
@@ -0,0 +1,24 @@
+<?php
+/**
+* Created By : Reaper45
+* Date : 23/06/2017
+* Time : 00:54 AM
+*
+* Local database connection configuration variables
+*
+* Rename this file to database.php
+* Repalce the following varibles with yours
+*
+*/
+
+
+ define('DB_HOST', '127.0.0.1' );
+
+ // Database name
+ define('DB_NAME', 'your_db_name' );
+
+ // Database username
+ define('DB_USER', 'your_db_username' );
+
+ // User's db password
+ define('DB_PASS', 'your_db_password' );
