f22abffda10eb684863b1ee6547f7c4036533b68
Reaper45 <jomwashighadi@gmail.com>
2017-06-23 01:40:13 +0300

    New class view set

---
+++ classes/View.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* Created By : Reaper45
+* Date : 23/06/2017
+* Time : 01:17 AM
+*
+*/
+
+// creating new views
+class View
+{
+  function __construct($data, $path)
+  {
+    require_once "{$path}.php";
+  }
+}
+
+
+/**
+* Class view complete as by: 01:23 AM
+* Do Not Modify this Infomation.
+*/
