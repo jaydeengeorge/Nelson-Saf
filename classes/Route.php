65d6d07083e3ebb691ae3b51ac708f4c3b41a00e
Reaper45 <jomwashighadi@gmail.com>
2017-06-24 18:57:11 +0300

    New class route

---
+++ classes/Route.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by Atom.
+ * User: reaper45
+ * Date: 06/24/17
+ * Time : 18:17 AM
+ */
+
+ class Route
+ {
+   // Store get routes [path => COntroller@method]
+   public static $_get = array();
+
+   // Store post routes [path => COntroller@method]
+   public static $_post = array();
+
+   function __construct()
+   {
+     # code...
+   }
+
+   public static function get($path, $action)
+   {
+     self::$_get["{$path}"] = $action;
+     return $this;
+   }
+
+   public static function post($value='')
+   {
+     # code...
+   }
+ }
