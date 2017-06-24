e24c3404c49305be83cda63d32d97f220393739a
Reaper45 <jomwashighadi@gmail.com>
2017-06-24 19:40:21 +0300

    Added 404 page

---
+++ views/404.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by Atom.
+ * User: reaper45
+ * Date: 06/23/17
+ * Time : 02:29 AM
+ *
+ */
+ // Page Header
+ require_once "templates/header.php";
+ ?>
+
+ <!-- Page content -->
+
+ <h1>404 Error. </h1>
+
+ <h3>Page Not Found!</h3>
+
+ <?
+
+ // page Footer
+ require_once "templates/footer.php";
+ ?>
