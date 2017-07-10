b4d58e27d1bc78a154657cf81043160a8078f371
Reaper45 <jomwashighadi@gmail.com>
2017-07-10 03:13:42 +0300

    Admins Controller

---
+++ controllers/AdminController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Author: <jomwashighadi@gmail.com>
+ * Date: 067/10/17
+ * Time : 02:59 AM
+ *
+ */
+ namespace controllers;
+
+ class AdminController extends Controller
+ {
+
+   function __construct($method, $parameters = null)
+   {
+     parent::__construct($method, $parameters);
+   }
+
+   public function index()
+   {
+     # code...
+   }
+ }
